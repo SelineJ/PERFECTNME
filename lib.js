b8484f6dcd81c5abd54211b3336afceb31f0f15c
SelineJ <69505295+SelineJ@users.noreply.github.com>
2020-10-03 00:55:28 +0200

    update

--- lib.js
+++ lib.js
@@ -5,29 +5,32 @@ showMain();
 function showMain(){
 document.getElementById('app').innerHTML = 
 `
-<h1>PERFECT NME</h1>
 <div class="fleks">
 
 <div class="header">
-<img class="logo" src="images/logo.jpg" alt="PERFECT NME"> </div>
+<img src="images/header.jpg" alt="PERFECT NME">
+</div>
     <div class="meny">
-        <nav>    
-                <ul>
-                    <button onclick="showMain()">HOME</button>
-                    <button onclick="showMain()">BAND</button>
-                    <button onclick="showMain()">RELEASES</button>
-                    <button onclick="showMain()">CONTACT</button>
-                </ul>
-        </nav>
+        
+                    <div onclick="showMain()">HOME</div>
+                    <div onclick="showMain()">BAND</div>
+                    <div onclick="showMain()">RELEASES</div>
+                    <div onclick="showMain()">CONTACT</div>
+        
+             
 </div>
 
-<div class="main">
-<aside>
-<img class="logo" src="images/side.jpg" alt="PERFECT NME">
-</aside>
+<main class="main">
+<article>
 
-</div>
-<div class="footer">footer</div>
+<h1>Overscrift</h1>
+<img class="logo" src="images/logo.jpg" alt="PERFECT NME">
+<p>hejehskhejkshekj hjeskhe sjkh sjhekhs kjheskj hejksan fkjd hfsejk</p>
+</article>
+
+</main>
+
+<div class="footer">HOME BAND RELEASES CONTACT</div>
 <div class="sidebar">sidebar</div>
 </div>
 
